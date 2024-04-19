9bed2af28505827ba1f27b0d42a51072310b04e2
frederik.lange@hotmail.com <frederik.lange@hotmail.com>
2024-04-19 22:22:46 +0200

    added leafnode child class to htmlnode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
 import unittest
 from htmlnode import (
     HTMLNode,
+    LeafNode,
 )
 
 class TestHTMLNode(unittest.TestCase):
@@ -24,6 +25,23 @@ class TestHTMLNode(unittest.TestCase):
     def test_repr(self):
         node = HTMLNode(tag="p", value="this is a test", props={"href": "https://www.boot.dev"})
         self.assertEqual(repr(node), "HTMLNode(p, this is a test, None, {'href': 'https://www.boot.dev'})")
+
+    def test_to_html_no_props(self):
+        leafnode = LeafNode(value="This is a paragraph of text.", tag="p")
+        self.assertEqual(leafnode.to_html(), "<p>This is a paragraph of text.</p>")
+
+    def test_to_html_with_props(self):
+        leafnode = LeafNode(value="Click me!", tag="a", props={"href": "https://www.google.com"})
+        self.assertEqual(leafnode.to_html(), '<a href="https://www.google.com">Click me!</a>') 
+
+    def test_to_html_no_value(self):
+        with self.assertRaises(ValueError):
+            leafnode = LeafNode(value=None, tag="a")
+            leafnode.to_html()
+    
+    def test_to_html_empty_value(self):
+        leafnode = LeafNode(value="", tag="p")
+        self.assertEqual(leafnode.to_html(), '<p></p>')
 
 if __name__ == "__main__":
     unittest.main()
