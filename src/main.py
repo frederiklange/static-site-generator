e5e78cb85933d242409f19e02de7d2744032a733
frederik-lange <frederik.lange@hotmail.com>
2024-05-24 09:11:43 +0200

    add page generation

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from textnode import TextNode
 from copy_static import copy_files_recursive
+from generate_page import generate_page
 import os, shutil
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 
 def main():
@@ -14,6 +17,14 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
+    print("Generating page...")
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html")
+    )
 
 if __name__ == "__main__":
     main()
+
+
